09845c6c4c64b3b859168c4298ee38768baab17b
Anthony Lawrence <A-Lawrence@users.noreply.github.com>
2017-04-21 14:58:04 +0100

    Fixed ESLint errors with lack of Path in Gulp.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ var phpspec = require('gulp-phpspec');
 var run = require('gulp-run');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
+var path = require('path');
 
 gulp.task('test', function() {
     gulp.src('spec/**/*.php')
@@ -11,12 +12,12 @@ gulp.task('test', function() {
         .on('error', notify.onError({
             title: "Fatal blow...",
             message: "If you're not failing, you're not trying hard enough.",
-            icon: __dirname + "/failure.png"
+            icon: path.join(__dirname, "/failure.png")
         }))
         .pipe(notify({
             title: "Elon would be proud!",
             message: "All green, and good to go!",
-            icon: __dirname + "/success.png"
+            icon: path.join(__dirname, "/success.png")
         }));
 });
 
